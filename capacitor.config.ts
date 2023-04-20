09920d62b1d906ab89a99f1181946208d2bb5b9c
unknown <juan.sueldo1989@gmail.com>
2023-04-20 16:45:12 -0300

    Splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,25 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'PruebaApp',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 500,
+      backgroundColor: "#FBCE04",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#FBCE04",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
